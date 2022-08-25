7b9a10e01fbd56dcf5c268166742d84292331241
SigherWolf <thomasw2701@gmail.com>
2022-08-25 17:31:03 +0100

    Created OpenFrame class. Open frames occur when there are pins left standing after both throws

---
+++ BowlingGame/OpenFrame.cs
@@ -0,0 +1,17 @@
+﻿namespace BowlingGame
+{
+    public class OpenFrame
+    {
+        int score;
+
+        public OpenFrame(int firstThrow, int secondThrow)
+        {
+            score = firstThrow + secondThrow;
+        }
+
+        public int Score()
+        {
+            return score;
+        }
+    }
+}
