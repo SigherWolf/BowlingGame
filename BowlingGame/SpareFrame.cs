5a3120afb0b55cef4fffb19efaab358182ebfd12
Tom Watson <thomasw2701@gmail.com>
2022-09-01 13:21:07 +0100

    Add files via upload

---
+++ BowlingGame/SpareFrame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+namespace BowlingGame
+{
+    // SpareFrame extends the abstract class Frame
+    public class SpareFrame : Frame
+    {
+        public SpareFrame(ArrayList throws, int startingThrow, int firstThrow, int secondThrow) : base(throws)
+        {
+            throws.Add(firstThrow);
+            throws.Add(secondThrow);
+        }
+
+        private int NextBall()
+        {
+            return (int) throws[startingThrow + 2];
+        }
+
+        override public int Score()
+        {
+            return 10 + NextBall();
+        }
+    }
+}
